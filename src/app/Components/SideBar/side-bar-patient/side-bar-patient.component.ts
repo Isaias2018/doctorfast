b9129557d17c0904a54a8b7c85e0941fb97421ba
Isaias Chamorro <42631531+Isaias2018@users.noreply.github.com>
2021-05-08 11:07:30 -0500

    v8

--- src/app/Components/SideBar/side-bar-patient/side-bar-patient.component.ts
+++ src/app/Components/SideBar/side-bar-patient/side-bar-patient.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Patient } from 'src/app/Model/Patient';
 import { AuthService } from 'src/app/Services/Auth/auth.service';
+import { PatientService } from 'src/app/Services/Patient/patient.service';
 import { SideBarPatientService } from 'src/app/Services/SideBarPatient/side-bar-patient.service';
 
 @Component({
@@ -9,20 +11,32 @@ import { SideBarPatientService } from 'src/app/Services/SideBarPatient/side-bar-
   styleUrls: ['./side-bar-patient.component.scss']
 })
 export class SideBarPatientComponent implements OnInit {
-  
-  activeMenu: boolean = false;
 
-  constructor(private authService: AuthService,private sideBarPatientService: SideBarPatientService, private router: Router) { }
+  activeMenu: boolean = false;
+  patient : Patient = new Patient;
+
+  constructor(private patientService: PatientService  ,private authService: AuthService,private sideBarPatientService: SideBarPatientService, private router: Router) { }
 
   ngOnInit(): void {
     this.sideBarPatientService.activeMenu.subscribe(data => {
       this.activeMenu = data;
     })
+    this.Nombre_Usuario();
   }
 
   public logout()
   {
     this.authService.clearToken();
     this.router.navigateByUrl('');
+  }
+
+
+  Nombre_Usuario(){
+
+    this.patientService.getPatientId()
+    .subscribe(data=>{
+      this.patient=data;
+    })
+
   }
 }
